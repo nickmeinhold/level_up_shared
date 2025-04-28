097237d94de85009726742bcfb752bb18035f9fb
Nick Meinhold <nick@enspyr.co>
2025-04-28 13:39:08 +1000

    Fix: add reps to RepsExerciseWithWeight's toJson

--- lib/src/workouts/models/exercise.dart
+++ lib/src/workouts/models/exercise.dart
@@ -128,6 +128,7 @@ class RepsExerciseWithWeight extends RepsExercise {
       'description': description,
       'videoUrl': videoUrl,
       'weight': weight,
+      'reps': reps,
       'sets': sets,
     };
   }
