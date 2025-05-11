740e1265508c87393c519e649a94bdbf5c1dbc4e
Nick Meinhold <nick@enspyr.co>
2025-05-11 18:23:02 +1000

    Add category field to Workouts

--- lib/src/workouts/models/workout.dart
+++ lib/src/workouts/models/workout.dart
@@ -1,11 +1,13 @@
 class Workout {
   const Workout({
     required this.id,
+    required this.category,
     required this.description,
     required this.exerciseIds,
   });
 
   final String id;
+  final int category;
   final String description;
   final List<String> exerciseIds;
 
@@ -16,8 +18,24 @@ class Workout {
   factory Workout.fromJsonWithId(String id, Map<String, Object?> json) {
     return Workout(
       id: id,
+      category: json['category'] as int,
       description: json['description'] as String,
       exerciseIds: List<String>.from(json['exerciseIds'] as List),
     );
+  }
+}
+
+enum WorkoutCategory { basketball, strength, fitness }
+
+extension WorkoutCategoryExtension on WorkoutCategory {
+  String get displayName {
+    switch (this) {
+      case WorkoutCategory.basketball:
+        return 'Basketball';
+      case WorkoutCategory.strength:
+        return 'Strength';
+      case WorkoutCategory.fitness:
+        return 'Fitness';
+    }
   }
 }
